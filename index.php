a4d069183edfa3b8af70fd9b9bdbf976ba812a1a
Felipe Andres Vidal Soto <felipeavidalsoto@gmail.com>
2022-06-16 10:56:53 -0300

    TEST: Prueba PR

--- index.php
+++ index.php
@@ -29,7 +29,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
     <script src="./js/app.js"></script>
-
+    <!-- Prueba pr -->
 </body>
 
 </html>
